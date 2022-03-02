688d684e2e36512b77d60e4c5876da3ef1c956ea
Alexandr E <ASErasov@stud.kantiana.ru>
2022-03-03 02:19:56 +0300

    add files

---
+++ Game_Erasov.cpp
@@ -0,0 +1,81 @@
+﻿#include <SFML/Graphics.hpp>
+#include "PLAYER.hpp"
+using namespace sf;
+
+
+class PLAYER;
+
+int main()
+{
+	RenderWindow window(VideoMode(600, 400), "GAME_TEST");
+
+	Texture t;
+	t.loadFromFile("ballb.png");
+
+	float currentFrame = 0;
+
+	PLAYER p(t);
+
+	Clock clock;
+
+	RectangleShape rectangle(Vector2f(32, 32));
+
+	while (window.isOpen())
+	{
+		float time = clock.getElapsedTime().asMicroseconds();
+		clock.restart();
+
+		time = time / 700;
+
+		if (time > 20) time = 20;
+
+		Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == Event::Closed)
+				window.close();
+		}
+
+		if (Keyboard::isKeyPressed(Keyboard::Left))
+		{
+			p.dx = -0.1;
+
+		}
+
+		if (Keyboard::isKeyPressed(Keyboard::Right))
+		{
+			p.dx = 0.1;
+		}
+
+		if (Keyboard::isKeyPressed(Keyboard::Up))
+		{
+			if (p.onGround) { p.dy = -0.35; p.onGround = false; }
+		}
+
+		p.update(time);
+
+		if (p.rect.left > 300) offsetX = p.rect.left - 300;
+		offsetY = p.rect.top - 200;
+
+		window.clear(Color::White);
+
+
+		for (int i = 0; i < H; i++)
+			for (int j = 0; j < W; j++)
+			{
+				if (TileMap[i][j] == 'B') rectangle.setFillColor(Color::Black);
+
+				if (TileMap[i][j] == '0')  rectangle.setFillColor(Color::Green);
+
+				if (TileMap[i][j] == ' ') continue;
+
+				rectangle.setPosition(j * 32 - offsetX, i * 32 - offsetY);
+				window.draw(rectangle);
+			}
+
+		window.draw(p.sprite);
+		window.display();
+	}
+
+	return 0;
+}
---
+++ inc/MAP.hpp
@@ -0,0 +1,25 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+
+using namespace sf;
+
+const int H = 12;
+const int W = 40;
+
+
+String TileMap[H] = {
+
+"BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB",
+"B                                      B",
+"B                                      B",
+"B                                      B",
+"B                            BBBB      B",
+"B         0000                         B",
+"B                   BBBB           BBBBB",
+"BBB                                    B",
+"B              BB                      B",
+"B              BB               BB     B",
+"B    BB        BB         BB    BB     B",
+"BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB",
+
+};
